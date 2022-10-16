b53e4c39f4ea38f0f61b387ffe4e1bb063401031
Ameer Badri <ameerbadri@gmail.com>
2022-10-16 21:08:18 +0100

    updated hpedge check plugin

--- plugins/netlify-check-hpedge-enabled/index.js
+++ plugins/netlify-check-hpedge-enabled/index.js
@@ -1,24 +1,39 @@
 // index.js
-const { spawn } = require("child_process");
+// const { spawn } = require("child_process");
+
+const { exec } = require("child_process");
   module.exports = {
     onPreBuild: () => {
       console.log("Hello world from onPreBuild event in netlify-check-hpedge-enabled");
       const ls = spawn("dig", ["www.netlifyhpedge.com"]);
 
-      ls.stdout.on("data", data => {
-          console.log(`stdout: ${data}`);
+      // ls.stdout.on("data", data => {
+      //     console.log(`stdout: ${data}`);
+      // });
+
+      // ls.stderr.on("data", data => {
+      //     console.log(`stderr: ${data}`);
+      // });
+
+      // ls.on('error', (error) => {
+      //     console.log(`error: ${error.message}`);
+      // });
+
+      // ls.on("close", code => {
+      //     console.log(`child process exited with code ${code}`);
+      // });
+
+      exec("host www.netlifyhpedge.com", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout/Netlify check hpedge Enabled: ${stdout}`);
       });
 
-      ls.stderr.on("data", data => {
-          console.log(`stderr: ${data}`);
-      });
-
-      ls.on('error', (error) => {
-          console.log(`error: ${error.message}`);
-      });
-
-      ls.on("close", code => {
-          console.log(`child process exited with code ${code}`);
-      });
           },
   }
