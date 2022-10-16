4d7aba1daee595f24dc26caeec200fe44d23f2b9
Ameer Badri <ameerbadri@gmail.com>
2022-10-16 21:36:34 +0100

    updated hpedge check plugin

--- plugins/netlify-check-hpedge-enabled/index.js
+++ plugins/netlify-check-hpedge-enabled/index.js
@@ -1,17 +1,18 @@
 // index.js
   // Accessing dns module
   //const dns = require('dns');
-  const { dns } = require("dns");
+  //const { dns } = require("dns");
 
   module.exports = {
     onPreBuild: () => {
       console.log("Hello world from onPreBuild event in netlify-check-hpedge-enabled");
     
-      // Set the rrtype for dns.resolve() method
-      const rrtype="CNAME";
-      // Calling dns.resolve() method for hostname
-      var w3 = dns.resolve('www.netlifyhpedge.com', rrtype, function (err, records) {
-        console.log('error: %j', err); 
-        console.log('records: %j', records) }); 
-          },
+      // // Set the rrtype for dns.resolve() method
+      // const rrtype="CNAME";
+      // // Calling dns.resolve() method for hostname
+      // var w3 = dns.resolve('www.netlifyhpedge.com', rrtype, function (err, records) {
+      //   console.log('error: %j', err); 
+      //   console.log('records: %j', records) }); 
+      //     
+    },
   }
