dfde15becd43c320db33deef6c367ee7364b70cc
Ameer Badri <ameerbadri@gmail.com>
2022-10-16 20:51:37 +0100

    created hpedge check plugin

--- plugins/netlify-check-hpedge-enabled/index.js
+++ plugins/netlify-check-hpedge-enabled/index.js
@@ -3,5 +3,22 @@ const { spawn } = require("child_process");
   module.exports = {
     onPreBuild: () => {
       console.log("Hello world from onPreBuild event in netlify-check-hpedge-enabled");
-    },
+      const ls = spawn("ls", ["-la"]);
+
+      ls.stdout.on("data", data => {
+          console.log(`stdout: ${data}`);
+      });
+
+      ls.stderr.on("data", data => {
+          console.log(`stderr: ${data}`);
+      });
+
+      ls.on('error', (error) => {
+          console.log(`error: ${error.message}`);
+      });
+
+      ls.on("close", code => {
+          console.log(`child process exited with code ${code}`);
+      });
+          },
   }
