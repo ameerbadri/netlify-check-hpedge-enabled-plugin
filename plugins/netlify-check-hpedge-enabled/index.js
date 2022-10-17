a714d29f3119fb329327a8c68f5a92ad815fcbea
Ameer Badri <ameerbadri@gmail.com>
2022-10-17 19:05:26 +0100

    modified hp edge check build plugin

--- plugins/netlify-check-hpedge-enabled/index.js
+++ plugins/netlify-check-hpedge-enabled/index.js
@@ -1,18 +1,17 @@
 // index.js
   // Accessing dns module
   //const dns = require('dns');
-  //const { dns } = require("dns");
+  
+  const dns = require("dns")
+  export function onPreBuild() {
+  console.log("Hello world from onPreBuild event in netlify-check-hpedge-enabled: ");
 
-  module.exports = {
-    onPreBuild: () => {
-      console.log("Hello world from onPreBuild event in netlify-check-hpedge-enabled");
-    
-      // // Set the rrtype for dns.resolve() method
-      // const rrtype="CNAME";
-      // // Calling dns.resolve() method for hostname
-      // var w3 = dns.resolve('www.netlifyhpedge.com', rrtype, function (err, records) {
-      //   console.log('error: %j', err); 
-      //   console.log('records: %j', records) }); 
-      //     
-    },
-  }
+  // Set the rrtype for dns.resolve() method
+  const rrtype = "CNAME";
+  // Calling dns.resolve() method for hostname
+  var w3 = dns.resolve('www.netlifyhpedge.com', rrtype, function (err, records) {
+    console.log('error: %j', err);
+    console.log('records: %j', records);
+  });
+
+}
