d0b098f5c65631c3acf5231f4e46bddaad39c46b
Henry Smith <henry.smith@netlify.com>
2022-10-18 10:39:02 +0100

    Updated plugin to check CNAME + A Record

--- plugins/netlify-check-hpedge-enabled/index.js
+++ plugins/netlify-check-hpedge-enabled/index.js
@@ -1,18 +1,59 @@
-// index.js
-  // Accessing dns module
-  var dns = require('node:dns');
+const { domain } = require("node:process");
 
-  module.exports = {
-    onPreBuild: async ({ inputs }) => {
+// Accessing dns module
+const { Resolver } = require("node:dns").promises;
+
+const checkDns = async (domain, rrtype) => {
+  resolver = new Resolver();
+  try {
+    const result = await resolver.resolve(domain, rrtype);
+    return result;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+module.exports = {
+  onPostBuild: async ({ inputs, utils }) => {
+    try {
       console.log(inputs);
-      // Set the rrtype for dns.resolve() method
-      const rrtype = "CNAME";
-      // Calling dns.resolve() method for hostname
-      dns.resolve(inputs.site_name, rrtype, (err, addresses) => {
-        console.log('error: %j', err);
-        console.log('addresses: %j', addresses);
-        console.log("Hello from onPreBuild event in netlify-check-hpedge-enabled: " + inputs.site_name);
-      });
+      const bareDomain = inputs.site_name.includes("www")
+        ? inputs.site_name.replace("www", "")
+        : `${inputs.site_name}`;
 
+      const wwwDomain = inputs.site_name.includes("www")
+        ? inputs.site_name
+        : `www.${inputs.site_name}`;
+
+      const cnameRecords = await checkDns(wwwDomain, "CNAME");
+      const aRecords = await checkDns(bareDomain, "A");
+
+      if (cnameRecords.length > 0 && aRecords.length > 0) {
+        const cnameCorrect = await cnameRecords.reduce((result, record) => {
+          return record.includes(".netlifyglobalcdn.com")
+            ? (result = true)
+            : result;
+        }, false);
+
+        const aCorrect = await aRecords.reduce((result, record) => {
+          return record.includes("147.75.40.150") ? (result = true) : result;
+        }, false);
+
+        if (cnameCorrect && aCorrect) {
+          console.log("Site is on Netlifys High Performance Edge");
+          return;
+        } else {
+          throw new Error();
+        }
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      console.log(error);
+      utils.build.failBuild(
+        "Build Failed - Site not on HP Edge - Check your DNS configuration"
+      );
     }
-  } 
+  },
+};
